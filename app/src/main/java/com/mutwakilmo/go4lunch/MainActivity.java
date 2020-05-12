adb7a5f5b0dacd619795ecadf36f3cd70bad83b0
mutwakilmo <mutwakilmo@gmail.com>
2020-05-12 15:00:04 +0200

    register my app with firebase

--- app/src/main/java/com/mutwakilmo/go4lunch/MainActivity.java
+++ app/src/main/java/com/mutwakilmo/go4lunch/MainActivity.java
@@ -1,11 +1,125 @@
 package com.mutwakilmo.go4lunch;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.content.Intent;
+import android.icu.text.DateFormat;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class MainActivity extends BaseActivity {
+    //For data
+    // 1 - Identifier for Sing-In Activity
+    public static final int RC_SIGN_IN  = 101;
+
+    //FOR DESIGN
+    // 1 - Get Coordinator Layout
+
+    @BindView(R.id.main_activity_coordinator_layout)
+    CoordinatorLayout coordinatorLayout;
+
+
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+    @Override
+    public int getFragmentLayout() { return R.layout.activity_main;
+    }
+
+
+
+    @OnClick(R.id.main_activity_button_login)
+    public void onClickLoginButton(){
+        //3 launch sing-in activity when user clicked on login button
+        this.startSignInActivity();
+    }
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    // 2 - Launch Sign-In Activity
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),
+                                        new AuthUI.IdpConfig.GoogleBuilder().build()))
+                        //setIsSmartLockEnabled()in order to automatically offer the user a list of
+                        // available emails to make it easier to register.
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+
+
+    // --------------------
+    // OnActivityResult
+    // --------------------
 
     @Override
-    public int getFragmentLayout() { return R.layout.activity_main; }
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Handle SingIn Activity response on activity result
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+
+
+    // --------------------
+    // UI
+    // --------------------
+
+    //show snack bar with a message
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+
+    // --------------------
+    // UTILS
+    // --------------------
+
+    //  Method that handles response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
+    }
 }
+
+
+
+
