bd34eef2a2ab00be63831df65cbbfb31b57f18e5
mutwakilmo <mutwakilmo@gmail.com>
2020-05-14 11:15:40 +0200

    OnboardingActivity creating dots

--- app/src/main/java/com/mutwakilmo/go4lunch/OnBoardingActivity.java
+++ app/src/main/java/com/mutwakilmo/go4lunch/OnBoardingActivity.java
@@ -1,9 +1,11 @@
 package com.mutwakilmo.go4lunch;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -28,6 +30,8 @@ public class OnBoardingActivity extends AppCompatActivity {
 
     SliderAdapter sliderAdapter;
 
+    TextView[] dotsL;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +41,45 @@ public class OnBoardingActivity extends AppCompatActivity {
         //call adapter
         sliderAdapter = new SliderAdapter(this);
         viewPager.setAdapter(sliderAdapter);
+        //for dots
+        addDots(0);
+        viewPager.addOnPageChangeListener(changeListener);
 
     }
 
+    private void addDots(int position){
+        dotsL = new TextView[4];
+        dots.removeAllViews();
+
+        for (int i= 0 ; i <dotsL.length; i++){
+            dotsL[i] = new TextView(this);
+            dotsL[i].setText(Html.fromHtml("&#8226"));
+            dotsL[i].setTextSize(35);
+            dots.addView(dotsL[i]);
+        }
+
+        if (dotsL.length > 0){
+            dotsL[position].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
+
+
+    //view dots
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+           addDots(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 
 }
